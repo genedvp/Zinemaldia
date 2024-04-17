5882776900a699fa3f5b3dd7bbbe7a90242a35b3
genedvp <149675326+genedvp@users.noreply.github.com>
2024-04-17 13:49:21 +0200

    estructura de base de datos

---
+++ bbdd/zinemaldiaBBDD.sql
@@ -0,0 +1,180 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-04-2024 a las 13:03:31
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `zinemaldia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `butacas`
+--
+
+CREATE TABLE `butacas` (
+  `id_butaca` int(11) NOT NULL,
+  `precio_butaca` int(11) DEFAULT NULL,
+  `columna` int(11) DEFAULT NULL,
+  `fila` int(11) DEFAULT NULL,
+  `id_sala` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `emisiones`
+--
+
+CREATE TABLE `emisiones` (
+  `id_sala` int(11) NOT NULL,
+  `id_pelicula` int(11) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `facturas`
+--
+
+CREATE TABLE `facturas` (
+  `id_butaca` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `precio_total` int(11) DEFAULT NULL,
+  `fecha_compra` date DEFAULT NULL,
+  `hora_compra` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `peliculas`
+--
+
+CREATE TABLE `peliculas` (
+  `id_pelicula` int(11) NOT NULL,
+  `titulo` varchar(100) DEFAULT NULL,
+  `director` varchar(100) DEFAULT NULL,
+  `descripcion` varchar(100) DEFAULT NULL,
+  `fecha_produccion` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `peliculas`
+--
+
+INSERT INTO `peliculas` (`id_pelicula`, `titulo`, `director`, `descripcion`, `fecha_produccion`) VALUES
+(1, 'La Naranja Mecánica', 'Stanley Kubrick', 'jeje', 1971),
+(2, 'Donnie Darko', 'Richard Kelly', 'rarita', 2001),
+(3, 'Her', 'Spike Jonze', 'bonita', 2013);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `salas`
+--
+
+CREATE TABLE `salas` (
+  `id_sala` int(11) NOT NULL,
+  `descripcion` varchar(100) DEFAULT NULL,
+  `capacidad` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `correo_electronico` varchar(100) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `apellidos` varchar(100) DEFAULT NULL,
+  `usuario` varchar(100) DEFAULT NULL,
+  `contrasenia` varchar(100) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  `rol` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `butacas`
+--
+ALTER TABLE `butacas`
+  ADD PRIMARY KEY (`id_butaca`);
+
+--
+-- Indices de la tabla `emisiones`
+--
+ALTER TABLE `emisiones`
+  ADD PRIMARY KEY (`id_pelicula`,`id_sala`),
+  ADD KEY `fk_id_emision_sala` (`id_sala`);
+
+--
+-- Indices de la tabla `facturas`
+--
+ALTER TABLE `facturas`
+  ADD PRIMARY KEY (`id_butaca`,`id_usuario`),
+  ADD KEY `fk_id_factura_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `peliculas`
+--
+ALTER TABLE `peliculas`
+  ADD PRIMARY KEY (`id_pelicula`);
+
+--
+-- Indices de la tabla `salas`
+--
+ALTER TABLE `salas`
+  ADD PRIMARY KEY (`id_sala`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `emisiones`
+--
+ALTER TABLE `emisiones`
+  ADD CONSTRAINT `fk_id_emision_pelicula` FOREIGN KEY (`id_pelicula`) REFERENCES `peliculas` (`id_pelicula`),
+  ADD CONSTRAINT `fk_id_emision_sala` FOREIGN KEY (`id_sala`) REFERENCES `salas` (`id_sala`);
+
+--
+-- Filtros para la tabla `facturas`
+--
+ALTER TABLE `facturas`
+  ADD CONSTRAINT `fk_id_factura_butaca` FOREIGN KEY (`id_butaca`) REFERENCES `butacas` (`id_butaca`),
+  ADD CONSTRAINT `fk_id_factura_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
