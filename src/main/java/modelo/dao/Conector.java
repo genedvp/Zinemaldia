f0baa94b3623ff7f7c2cd29832eea62d0bbc48be
genedvp <149675326+genedvp@users.noreply.github.com>
2024-04-30 09:31:37 +0200

    conector

--- src/main/java/modelo/dao/Conector.java
+++ src/main/java/modelo/dao/Conector.java
@@ -1,42 +1,26 @@
 package modelo.dao;
-import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class Conector {
 	
 	protected Connection conexion;
-	Statement statement;
+	java.sql.Statement statement;
 
 	/**
 	 */
 
 	Conector() {
 			try {	             
-	              String url = "jdbc:mysql://192.168.1.20:3306/zinemaldia?user=root&password=1324"; 
-	              Class.forName("com.mysql.jdbc.Driver");
-	              this.conexion = DriverManager.getConnection(url);	  
-	              statement = (Statement) conexion.createStatement();
+	              Class.forName("com.mysql.cj.jdbc.Driver");
+	              this.conexion = DriverManager.getConnection("jdbc:mysql://" + Config.HOST + "/" + Config.BBDD, Config.USERNAME, Config.PASSWORD);
 	              System.out.println("Mysql connected");
-	              
-				//this.conexion = DriverManager.getConnection("jdbc:mysql://" + Config.HOST + "/" + Config.BBDD, Config.USERNAME, Config.PASSWORD);
+				
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-			
-		
 	}
-
-	public Connection getConexion() {
-		return conexion;
-	}
-
-	public void setConexion(Connection conexion) {
-		this.conexion = conexion;
-	}
-	
 }
--- src/main/java/modelo/dao/Config.java
+++ src/main/java/modelo/dao/Config.java
@@ -3,8 +3,8 @@ package modelo.dao;
 public interface Config {
 
 	public static final String BBDD = "zinemaldia";
-	public static final String HOST = "localhost";
+	public static final String HOST = "192.168.1.20:33060";
 	public static final String USERNAME = "root";
-	public static final String PASSWORD = "";
+	public static final String PASSWORD = "secret";
 
 }
